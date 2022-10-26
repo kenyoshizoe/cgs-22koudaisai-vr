455472aa475dc571f585645b36673335fb5928ba
imo-abura <070yasuda5584@gmail.com>
2022-10-27 06:31:49 +0900

    add Switch and LEDSwitch.cs

---
+++ Assets/Project/Scripts/LEDSwitch.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 触れたらLEDが点灯する
+public class LEDSwitch : MonoBehaviour
+{
+	public string tagName;
+	public string showObjectName;  
+	GameObject showObject;
+
+	void Start() 
+	{
+		showObject = GameObject.Find(showObjectName);
+		if (showObject) // 見つけたら
+		{
+			showObject.SetActive(false); // 消す
+		}
+	}
+
+	
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == tagName)
+            {
+				showObject.SetActive(true); // 消していたものを点灯する
+            
+            }
+	}
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject.tag == tagName)
+            {
+				showObject.SetActive(false); // 点灯したものを消す
+            
+            }
+	}
+}

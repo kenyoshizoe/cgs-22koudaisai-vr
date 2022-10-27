4a0bc25956d77db8ae23abacb4a7931b821b8db5
Ken Yoshizoe <kenyoshizoe@gmail.com>
2022-10-27 23:23:10 +0900

    update solder hole

--- Assets/Project/Scripts/SolderHole.cs
+++ Assets/Project/Scripts/SolderHole.cs
@@ -5,9 +5,7 @@ using UnityEngine;
 public class SolderHole : MonoBehaviour
 {
     [SerializeField]
-    Collider targetFootCollider;
-    [SerializeField]
-    GameObject targetFoot;
+    List<Collider> targetFootColliders;
     [SerializeField]
     Collider solderingIron;
     [SerializeField]
@@ -15,33 +13,68 @@ public class SolderHole : MonoBehaviour
     [SerializeField]
     GameObject solder;
 
+    [SerializeField]
+    bool debug;
+    [SerializeField]
+    Material debugRedMaterial;
+    [SerializeField]
+    Material debugGreenMaterial;
+    [SerializeField]
+    Material debugBlueMaterial;
     public float solderingTime;
     float t;
 
     public bool isInserted;
 
+    bool m_isSoldered;
     public bool isSoldered
     {
-        get { return isSoldered; }
+        get { return m_isSoldered; }
         set
         {
-            // show up solder
-            isSoldered = value;
+            solder.SetActive(value);
+            m_isSoldered = value;
         }
     }
 
     public bool footTouching = false;
     bool solderingIronTouching = false;
     bool solderingWireTouching = false;
+    GameObject debugBall;
 
     void Start()
     {
         isInserted = false;
         isSoldered = false;
+        if (debug)
+        {
+            debugBall = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            debugBall.transform.SetParent(this.transform);
+            debugBall.transform.localPosition = new Vector3(0.0f, 0.0f, -0.03f);
+            debugBall.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+            debugBall.GetComponent<Renderer>().material = debugGreenMaterial;
+        }
+        gameObject.GetComponentInChildren<ParticleSystem>().Stop();
     }
 
     void Update()
     {
+        if (debug)
+        {
+            if (isSoldered)
+            {
+                debugBall.GetComponent<Renderer>().material = debugRedMaterial;
+            }
+            else if (solderingWireTouching)
+            {
+                debugBall.GetComponent<Renderer>().material = debugBlueMaterial;
+            }
+            else
+            {
+                debugBall.GetComponent<Renderer>().material = debugGreenMaterial;
+            }
+        }
+
         if (isInserted && solderingIronTouching && solderingWireTouching)
         {
             // count up
@@ -50,30 +83,32 @@ public class SolderHole : MonoBehaviour
             {
                 isSoldered = true;
             }
+            gameObject.GetComponentInChildren<ParticleSystem>().Play();
         }
         else
         {
             t = 0;
+            gameObject.GetComponentInChildren<ParticleSystem>().Stop();
         }
     }
 
-    void OnCollisionEnter(Collision other)
+    void OnTriggerEnter(Collider other)
     {
-        if (other.collider == targetFootCollider)
+        if (targetFootColliders.Contains(other))
             footTouching = true;
-        else if (other.collider == solderingIron)
+        else if (other == solderingIron)
             solderingIronTouching = true;
-        else if (other.collider == solderingWire)
+        else if (other == solderingWire)
             solderingWireTouching = true;
     }
 
-    void OnCollisionExit(Collision other)
+    void OnTriggerExit(Collider other)
     {
-        if (other.collider == targetFootCollider)
+        if (targetFootColliders.Contains(other))
             footTouching = false;
-        else if (other.collider == solderingIron)
+        else if (other == solderingIron)
             solderingIronTouching = false;
-        else if (other.collider == solderingWire)
+        else if (other == solderingWire)
             solderingWireTouching = false;
     }
 
